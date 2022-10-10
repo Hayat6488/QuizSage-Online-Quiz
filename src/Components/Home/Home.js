33752f621364e0177aee22ec01116453b0e48465
Hayat6488 <abulhayat619@gmail.com>
2022-10-11 00:34:37 +0600

    header text with bg added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div className='mt-20'>
-            <p>Hayat</p>
+        <div>
+            <div className='bg text-center p-48 text-left mb-20'>
+                <p className='text-white text-4xl font-semibold text-red-500'>Test Your Knowledge...</p>
+                <p className='text-4xl text-white font-semibold text-wrap mr-96 mt-4 text-yellow-400'>Try our topic based quiz tests to test your knowledge and skills. And make the best preparation for your upcoming competition and career. Select your favourite topic to participate. And know about the depth of your learning.</p>
+            </div>
         </div>
+
     );
 };
 
