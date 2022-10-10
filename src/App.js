ad4e2b0ff7be4b8f60f88b375ba35e95b6144c88
Hayat6488 <abulhayat619@gmail.com>
2022-10-10 23:49:46 +0600

    Route setup complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layout/Main';
+import Home from './Components/Home/Home';
+import Topics from './Components/Topics/Topics';
+import Statistics from './Components/Statistics/Statistics';
+import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/', element: <Home></Home>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blogs',
+          element: <Blogs></Blogs>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        }
+      ]
+    }
+])
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='px-20 flex justify-between bg-cyan-600 py-4 items-center'>
+            <div>
+                <p className='text-orange-400 text-4xl font-semibold'>QuizSage</p>
+            </div>
+            <div className='text-orange-400'>
+                <Link className='mr-6 text-2xl font-semibold' to='/'>Home</Link>
+                <Link className='mr-6 text-2xl font-semibold' to='/statistics'>Statistics</Link>
+                <Link className='mr-6 text-2xl font-semibold' to='/blogs'>Blogs</Link>
+                <Link className='mr-6 text-2xl font-semibold' to='/about'>About Us</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
