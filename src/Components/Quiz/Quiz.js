ef6ec3ee2d5ce731c8da8d0e22277c9f88219a42
Hayat6488 <abulhayat619@gmail.com>
2022-10-11 05:41:11 +0600

    quiz added

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Quiz = ({quiz}) => {
     console.log(quiz);
+    const ques = quiz.question.replace(/(<([^>]+)>)/ig, '');
+    
     return (
-        <div>
-            {quiz.question}
+        <div className='bg-slate-800 p-4'>
+            <p className='text-yellow-400 text-2xl mb-2'>{ques}</p>
+            <div className='grid grid-cols-2 gap-4 '>
+            {
+                quiz.options.map(option => <div className='w-60 bg-slate-600 px-2 py-1 text-orange-400 hover:bg-zinc-800 hover:text-yellow-400'><button>{option}</button></div>)
+            }
+            </div>
         </div>
     );
 };
--- src/Components/Quizes/Quizes.js
+++ src/Components/Quizes/Quizes.js
@@ -4,14 +4,16 @@ import Quiz from '../Quiz/Quiz';
 
 const Quizes = () => {
     const quizes = useLoaderData();
-    const {name, questions} = quizes.data;
+    const { name, questions } = quizes.data;
     console.log(name, questions);
     return (
-        <div>
-            <p>{name}</p>
-            {
-                questions.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)
-            }
+        <div className='mt-20'>
+            <p className='text-orange-400 text-3xl font-semibold bg-slate-800 py-4 mb-4'>{name}</p>
+            <div className='grid md:grid-cols-1 lg:grid-cols-2 gap-4 mx-16'>
+                {
+                    questions.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)
+                }
+            </div>
         </div>
     );
 };
