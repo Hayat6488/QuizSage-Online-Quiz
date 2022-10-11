c373f6f62171bf955284ae42e2e37703e0e51d34
Hayat6488 <abulhayat619@gmail.com>
2022-10-12 01:36:13 +0600

    Line chart ADDED

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const apiData = useLoaderData();
+    const data = apiData.data;
+    console.log(data);
     return (
-        <div>
-            <p>Statistics</p>
+        <div className='flex justify-center items-center mt-20 flex-col'>
+            <LineChart width={500} height={400} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <Line type="monotone" dataKey="total" stroke="#FB923C" />
+            <XAxis dataKey="name" stroke='#FACC15' padding={{ left: 30, right: 30 }} />
+          <YAxis stroke='#FACC15' />
+          <Legend></Legend>
+          <Tooltip></Tooltip>
+            </LineChart>
+            <p className='break-normal mt-8 text-2xl font-semibold text-orange-400'>Fig: Line Chart Showing Topic and Number of Questions</p>
         </div>
     );
 };
