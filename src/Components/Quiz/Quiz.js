b1100598219acf2955a640de68d65c12c98128d7
Hayat6488 <abulhayat619@gmail.com>
2022-10-11 23:56:11 +0600

    show correct and on eye click added

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -3,6 +3,11 @@ import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Quiz = ({ quiz, setAnswered, answered, setCorrect, correct }) => {
     const [state, setState] = useState(false);
+    const [show,setShow] = useState(true);
+
+    const handleHidden = () => {
+        setShow(!show);
+    }
     
     const handleClick = (answer) => {
         if(quiz.correctAnswer===answer){
@@ -21,7 +26,7 @@ const Quiz = ({ quiz, setAnswered, answered, setCorrect, correct }) => {
         <div className='bg-slate-800 p-4'>
             <div className='flex justify-between items-center'>
                 <p className='text-yellow-400 text-2xl mb-2 break-normal'>{ques}</p>
-                <button><p><EyeIcon className='h-6 w-6 text-yellow-400 break-normal' /></p></button>
+                <button onClick={()=>handleHidden()}><p><EyeIcon className='h-6 w-6 text-yellow-400 break-normal' /></p></button>
             </div>
             <div className='grid grid-cols-2 gap-4 '>
                 {
@@ -29,7 +34,7 @@ const Quiz = ({ quiz, setAnswered, answered, setCorrect, correct }) => {
                     quiz.options.map(option => <button className='bg-slate-600 px-2 py-1 text-orange-400 hover:bg-zinc-800 hover:text-yellow-400 break-normal' disabled={state} onClick={() => handleClick(option)}>{option}</button>)
                 }
             </div>
-            <div className='mt-2'><p className='break-normal text-xl text-yellow-400'><span className='text-orange-400'>Correct Answer: </span>{quiz.correctAnswer}</p></div>
+            <div  hidden={show} className='mt-2'><p className='break-normal text-xl text-yellow-400'><span className='text-orange-400'>Correct Answer: </span>{quiz.correctAnswer}</p></div>
         </div>
     );
 };
