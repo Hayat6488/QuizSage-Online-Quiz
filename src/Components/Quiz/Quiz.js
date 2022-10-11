2831c02364fc693f3a3103be1868416faadabd49
Hayat6488 <abulhayat619@gmail.com>
2022-10-11 23:45:51 +0600

    results added

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { EyeIcon } from '@heroicons/react/24/solid'
 
-const Quiz = ({ quiz }) => {
+const Quiz = ({ quiz, setAnswered, answered, setCorrect, correct }) => {
+    const [state, setState] = useState(false);
+    
     const handleClick = (answer) => {
         if(quiz.correctAnswer===answer){
-            alert('Answer is Correct...')
+            alert('Answer is Correct...');
+            setCorrect(correct + 1);
         }
         else(
             alert('Answer is Incorrect')
         )
+        setState(!state);
+        setAnswered(answered+1);
         }
     const ques = quiz.question.replace(/(<([^>]+)>)/ig, '');
 
@@ -21,7 +26,7 @@ const Quiz = ({ quiz }) => {
             <div className='grid grid-cols-2 gap-4 '>
                 {
                     // quiz.options.map(option => <div className='bg-slate-600 px-2 py-1 text-orange-400 hover:bg-zinc-800 hover:text-yellow-400 break-normal'><input className='text-yellow-400' type='radio' id={quiz.id} name={quiz.id}onClick={() => handleClick({option})}></input><label for={quiz.id}> {option}</label></div>)
-                    quiz.options.map(option => <button className='bg-slate-600 px-2 py-1 text-orange-400 hover:bg-zinc-800 hover:text-yellow-400 break-normal' onClick={() => handleClick(option)}>{option}</button>)
+                    quiz.options.map(option => <button className='bg-slate-600 px-2 py-1 text-orange-400 hover:bg-zinc-800 hover:text-yellow-400 break-normal' disabled={state} onClick={() => handleClick(option)}>{option}</button>)
                 }
             </div>
             <div className='mt-2'><p className='break-normal text-xl text-yellow-400'><span className='text-orange-400'>Correct Answer: </span>{quiz.correctAnswer}</p></div>
--- src/Components/Quizes/Quizes.js
+++ src/Components/Quizes/Quizes.js
@@ -4,6 +4,9 @@ import Quiz from '../Quiz/Quiz';
 
 const Quizes = () => {
     const quizes = useLoaderData();
+    const [answered, setAnswered] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    console.log(correct);
     
     const { name, questions } = quizes.data;
     return (
@@ -12,13 +15,13 @@ const Quizes = () => {
             <div className='flex flex-col sm:flex-col md:flex-col lg:flex-row xl:flex-row 2xl:flex-row'>
                 <div className='grid md:grid-cols-1 lg:grid-cols-2 gap-4 mx-16 w-3/4'>
                     {
-                        questions.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)
+                        questions.map(quiz => <Quiz key={quiz.id} quiz={quiz} setAnswered={setAnswered} answered={answered} setCorrect={setCorrect} correct={correct}></Quiz>)
                     }
                 </div>
                 <div className='bg-slate-800 p-4 text-left text-xl text-yellow-400'>
                     <p className='mb-2 break-normal'>Results</p>
-                    <p className='mb-2 break-normal'>Number of Questions Answered: </p>
-                    <p className='break-normal'>Number of Correct Answers: </p>
+                    <p className='mb-2 break-normal'>Number of Questions Answered: {answered}</p>
+                    <p className='break-normal'>Number of Correct Answers: {correct}</p>
                 </div>
             </div>
         </div>
