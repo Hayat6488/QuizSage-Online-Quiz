8ba0f5e467ecbc232ff374e835d28163f60f00cc
Hayat6488 <abulhayat619@gmail.com>
2022-10-12 16:21:55 +0600

    about done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import pic from '../../images/UnderConstruction.png'
 
 const About = () => {
     return (
-        <div>
-            <p>This is about.</p>
+        <div className='flex justify-center items-center'>
+            <img className='w-1/2' src={pic} alt="" />
         </div>
     );
 };
